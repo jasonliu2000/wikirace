ca134d6044b87e0a70c6ac7e9dfdc014916ba16c
Jason Liu <jasonliu00@gmail.com>
2023-08-22 22:18:17 -0400

    Make http connection and read from input

--- WikiPage.java
+++ WikiPage.java
@@ -1,5 +1,39 @@
+import java.net.URL;
+import java.net.HttpURLConnection;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class WikiPage {
-  public static void main (String[] args) {
-    System.out.println("Hello world");
-  }
+
+	public static void main (String[] args) {
+		try {
+		URL url = new URL("https://en.wikipedia.org/wiki/Wikiracing");
+
+		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+		connection.setRequestMethod("GET");
+
+		int responseCode = connection.getResponseCode();
+		if (responseCode != HttpURLConnection.HTTP_OK) {
+			System.out.println("HTTP request failed with response code: " + responseCode);
+
+		} else {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+			String line;
+			StringBuilder content = new StringBuilder();
+
+			while ((line = reader.readLine()) != null) {
+				content.append(line);
+			}
+
+			reader.close();
+            connection.disconnect();
+
+			System.out.println(content.toString());
+		}
+
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
