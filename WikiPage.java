9d3d2bf73f36ee299ee5631000e74c52f165cd62
Jason Liu <jasonliu00@gmail.com>
2023-08-25 20:22:20 -0400

    Refactor to use Jsoup

--- WikiPage.java
+++ WikiPage.java
@@ -3,33 +3,16 @@ import java.net.HttpURLConnection;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+import org.jsoup.nodes.Document;
+import org.jsoup.Jsoup;
+
 public class WikiPage {
 
 	public static void main (String[] args) {
 		try {
-			URL url = new URL("https://en.wikipedia.org/wiki/Wikiracing");
-
-			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-			connection.setRequestMethod("GET");
-
-			int responseCode = connection.getResponseCode();
-			if (responseCode != HttpURLConnection.HTTP_OK) {
-				System.out.println("HTTP request failed with response code: " + responseCode);
-
-			} else {
-				BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-
-				String line;
-				StringBuilder content = new StringBuilder();
-
-				while ((line = reader.readLine()) != null) {
-					content.append(line);
-				}
-
-				reader.close();
-				connection.disconnect();
-
-				System.out.println(content.toString());
+				Document doc = Jsoup.connect("https://en.wikipedia.org/wiki/Wikiracing").get();
+				String body = doc.body().text();
+				System.out.println(body);
 			}
 		} catch(Exception e) {
 			e.printStackTrace();
