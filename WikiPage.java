159a77592565334531f63903b27415d31a192fb1
Jason Liu <jasonliu00@gmail.com>
2023-08-26 16:08:55 -0400

    Refactor WikiPage

--- WikiPage.java
+++ WikiPage.java
@@ -3,16 +3,25 @@ import java.net.HttpURLConnection;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.List;
+import java.util.LinkedList;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.*;
 import org.jsoup.select.Elements;
 
 public class WikiPage {
+	String wikiBaseUrl = "https://en.wikipedia.org";
+	String wikiSubPath = "/wiki/";
 
-	public static void main (String[] args) {
+	private LinkedList<String> links = new LinkedList<String>();
+	String title;
+
+	public WikiPage(String endPath) {
+		title = endPath;
+		String url = wikiBaseUrl + wikiSubPath + endPath;
+
 		try {
-				Document doc = Jsoup.connect("https://en.wikipedia.org/wiki/Wikiracing").get();
+				Document doc = Jsoup.connect(url).get();
 				Element body = doc.body();
 
 				Elements paragraphs = body.select("p");
@@ -20,17 +29,22 @@ public class WikiPage {
 					Elements hyperlinks = p.select("a");
 
 					List<String> urls = hyperlinks.eachAttr("href");
-					urls.removeIf(u -> !u.startsWith("/wiki"));
+					urls.removeIf(u -> !u.startsWith(wikiSubPath));
 					urls.removeIf(u -> u.endsWith("Citation_needed"));
-					// TODO: consider removing subcategory part in urls
+					// TODO: consider removing subcategory fragment in urls
 					// ex. "/wiki/White_paper#In_government" should be changed to "/wiki/White_paper"
 
 					for (String u : urls) {
-						System.out.println(u);
+						String linkName = u.substring(wikiSubPath.length());
+						links.addLast(linkName);
 					}
 				}
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+	public LinkedList<String> getLinks() {
+		return links;
 	}
 }
