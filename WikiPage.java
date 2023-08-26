4d6fe23d2863c2b39ca44f8ad477dbd3f8ec454d
Jason Liu <jasonliu00@gmail.com>
2023-08-25 22:05:26 -0400

    Print out internal wiki links in article body

--- WikiPage.java
+++ WikiPage.java
@@ -2,17 +2,33 @@ import java.net.URL;
 import java.net.HttpURLConnection;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.List;
 
-import org.jsoup.nodes.Document;
 import org.jsoup.Jsoup;
+import org.jsoup.nodes.*;
+import org.jsoup.select.Elements;
 
 public class WikiPage {
 
 	public static void main (String[] args) {
 		try {
 				Document doc = Jsoup.connect("https://en.wikipedia.org/wiki/Wikiracing").get();
-				String body = doc.body().text();
-				System.out.println(body);
+				Element body = doc.body();
+
+				Elements paragraphs = body.select("p");
+				for (Element p : paragraphs) {
+					Elements hyperlinks = p.select("a");
+
+					List<String> urls = hyperlinks.eachAttr("href");
+					urls.removeIf(u -> !u.startsWith("/wiki"));
+					urls.removeIf(u -> u.endsWith("Citation_needed"));
+					// TODO: consider removing subcategory part in urls
+					// ex. "/wiki/White_paper#In_government" should be changed to "/wiki/White_paper"
+
+					for (String u : urls) {
+						System.out.println(u);
+					}
+				}
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
