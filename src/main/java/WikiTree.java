1534e9f17225f86ed1e150ae9c8587eef88a5fa4
Jason Liu <jasonliu00@gmail.com>
2023-10-14 21:55:44 -0400

    Print search path in user-friendly way

--- src/main/java/WikiTree.java
+++ src/main/java/WikiTree.java
@@ -3,7 +3,7 @@ import java.util.LinkedList;
 public class WikiTree {
   long startTime;
   String startingPage;
-  String targetPage;
+  String destinationPage;
   WikiNode currentNode;
 
   int level = 0;
@@ -11,7 +11,7 @@ public class WikiTree {
   public WikiTree(String start, String finish) {
     startTime = System.currentTimeMillis();
     startingPage = start;
-    targetPage = finish;
+    destinationPage = finish;
 
     WikiNode startNode = new WikiNode(startingPage);
     Backlog.add(startNode);
@@ -28,7 +28,7 @@ public class WikiTree {
       currentNode = Backlog.pop();
       System.out.println(" -- Current wiki page: " + currentNode.name);
 
-      if (currentNode.name.equals(targetPage)) {
+      if (currentNode.name.equals(destinationPage)) {
         return successMessage(currentNode.pathToNode);
       }
 
@@ -39,11 +39,21 @@ public class WikiTree {
     return search();
   }
 
-  private String successMessage(LinkedList<String> path) {
+  private String successMessage(LinkedList<String> visitedNodes) {
     String time = String.format("Time taken: %s ms", System.currentTimeMillis() - startTime);
 
     String linkString = (level == 1) ? "link" : "links";
-    String message = String.format("%s and %s are %s %s away", startingPage, targetPage, String.valueOf(level), linkString);
+    String message = String.format("%s and %s are %s %s away", startingPage, destinationPage, String.valueOf(level), linkString);
+
+    String path = "Path taken to reach destination wiki page: ";
+    String[] pagesVisited = new String[visitedNodes.size()];
+    for (int i = 0; i < pagesVisited.length; i++) {
+      if (i < pagesVisited.length - 1) {
+        path += visitedNodes.pop() + " -> ";
+      } else {
+        path += visitedNodes.pop();
+      }
+    }
 
     return String.format("%s\n%s\n%s", time, message, path);
   }
