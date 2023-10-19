878b9b856a636e9bf42241942599793a8891f115
Jason Liu <jasonliu00@gmail.com>
2023-10-19 19:44:56 -0400

    Add unit tests for WikiPage.exists()

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,15 +1,31 @@
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import org.jsoup.HttpStatusException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class AppTest 
-{
+{   
+    @Test
+    @DisplayName("Return false if wiki article does not exist")
+    public void returnFalseIfWikiArticleExists()
+    {
+        assertFalse( WikiPage.exists("nonExistentArticle") );
+    }
+
+    @Test
+    @DisplayName("Return true if wiki article does not exist")
+    public void returnTrueIfWikiArticleExists()
+    {
+        assertTrue( WikiPage.exists("Wikipedia") );
+    }
+
     @Test
     @DisplayName("Get links in wikipedia article 'Wikiracing'")
     public void getWikiLinks()
     {
-        WikiPage wikiPage = new WikiPage();
-        assertTrue( wikiPage.getLinks("Wikiracing").size() > 0 );
-        assertTrue( wikiPage.getLinks("Wikiracing").contains("Wikipedia") );
+        assertTrue( WikiPage.getLinks("Wikiracing").size() > 0 );
+        assertTrue( WikiPage.getLinks("Wikiracing").contains("Wikipedia") );
     }
 }
