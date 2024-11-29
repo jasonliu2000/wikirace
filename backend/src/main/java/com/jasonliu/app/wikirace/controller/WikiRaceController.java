dd4699d429f80b6b61d53893acb7a5edae296b3a
Jason Liu <jasonliu00@gmail.com>
2024-11-29 17:28:00 -0500

    Clean up imports

--- backend/src/main/java/com/jasonliu/app/wikirace/controller/WikiRaceController.java
+++ backend/src/main/java/com/jasonliu/app/wikirace/controller/WikiRaceController.java
@@ -11,13 +11,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.ArrayList;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
