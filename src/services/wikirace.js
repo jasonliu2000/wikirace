b60c3b6263d46da99ba1477a0a5185c62a687fd3
Jason Liu <jasonliu00@gmail.com>
2024-12-03 22:24:49 -0500

    Refactor axios base URL

--- src/services/wikirace.js
+++ src/services/wikirace.js
@@ -1,19 +1,21 @@
 import axios from 'axios';
 
-const baseURL = '/api';
+const apiClient = axios.create({
+  baseURL: '/api',
+});
 
 async function getAll() {
-  const response = await axios.get(`${baseURL}/wikiraces`);
+  const response = await apiClient.get('/wikiraces');
   return response.data;
 }
 
 async function get(wikiRaceLocation) {
-  const response = await axios.get(`${baseURL}${wikiRaceLocation}`)
+  const response = await apiClient.get(wikiRaceLocation)
   return response.data;
 }
 
 async function start(wikiRaceToInitiate) {
-  return axios.post(`${baseURL}/wikirace`, wikiRaceToInitiate);
+  return apiClient.post('/wikirace', wikiRaceToInitiate);
 }
 
 export default { getAll, get, start };
