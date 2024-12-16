cfef690f9852f66db76b2ee2dc7de9b68e0444d3
Jason Liu <jasonliu00@gmail.com>
2024-12-15 22:45:13 -0500

    Add retry mechanism if server returns 429 error

--- backend/src/main/java/com/jasonliu/app/wikirace/wiki/WikiPage.java
+++ backend/src/main/java/com/jasonliu/app/wikirace/wiki/WikiPage.java
@@ -1,12 +1,14 @@
 package com.jasonliu.app.wikirace.wiki;
 
 import java.util.LinkedList;
+import java.util.Map;
 import java.util.logging.Logger;
 
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.*;
 import org.jsoup.select.Elements;
+import org.jsoup.HttpStatusException;
 
 import java.io.IOException;
 
@@ -33,43 +35,68 @@ public class WikiPage {
 	public static LinkedList<String> getLinks(String page) {
 		String url = getUrl(page);
 		LinkedList<String> links = new LinkedList<String>();
+		int attempts = 0, maxRetries = 3, retryDelay = 0;
+		boolean exit = false;
+		Connection.Response response;
 		
-		try {
-			Document doc = Jsoup.connect(url).get();
-			Element body = doc.body()
+		while (attempts < maxRetries) {
+			try {
+				Thread.sleep(retryDelay);
+				response = Jsoup.connect(url).ignoreHttpErrors(true).execute();
+
+				if (response.statusCode() == 429) {
+					logger.severe(String.format("Received 429 error from server after attempting to get links from article %s (attempt %s)", page, Integer.toString(attempts)+1));
+					retryDelay = Integer.parseInt(response.headers().get("retry-after")) * 1000;
+					attempts++;
+
+				} else if (response.statusCode() != 200) {
+					throw new HttpStatusException(response.statusMessage(), response.statusCode(), url);
+
+				} else {
+					Element body = response.parse()
+												.body()
 												.select("#mw-content-text .mw-parser-output")
 												.first();
-												
-			if (body.selectFirst(".infobox") != null) {
-				body.selectFirst(".infobox").remove();
-			}
+										
+					if (body.selectFirst(".infobox") != null) {
+						body.selectFirst(".infobox").remove();
+					}
 
-			Elements notes = body.select("div[role=note]");
-			for (Element note : notes) {
-				note.remove();
-			}
-			
-			Element terminateTag = body.selectFirst("#See_also");
-			if (terminateTag == null) {
-				terminateTag = body.selectFirst("#Notes");
-			}
-			if (terminateTag == null) {
-				terminateTag = body.selectFirst("#References");
-			}
-			if (terminateTag != null) {
-				terminateTag.parent().nextElementSiblings().remove();
-			}
+					Elements notes = body.select("div[role=note]");
+					for (Element note : notes) {
+						note.remove();
+					}
+					
+					Element terminateTag = body.selectFirst("#See_also");
+					if (terminateTag == null) {
+						terminateTag = body.selectFirst("#Notes");
+					}
+					if (terminateTag == null) {
+						terminateTag = body.selectFirst("#References");
+					}
+					if (terminateTag != null) {
+						terminateTag.parent().nextElementSiblings().remove();
+					}
 
-			Elements aTags = body.getElementsByAttributeValueMatching("href", aTagsRegex);
+					Elements aTags = body.getElementsByAttributeValueMatching("href", aTagsRegex);
 
-			for (Element a : aTags) {
-				String urlPath = a.attr("href");
-				urlPath = urlPath.substring(wikiSubPath.length());
-				links.add(urlPath);
+					for (Element a : aTags) {
+						String urlPath = a.attr("href");
+						urlPath = urlPath.substring(wikiSubPath.length());
+						links.add(urlPath);
+					}
+
+					if (attempts > 0) {
+						logger.info(String.format("Got links from wiki article %s after previous 429 errors", page));
+					}
+
+					break;
+				}
+			} catch(Exception e) {
+				logger.severe(String.format("error here: %s", e.getMessage()));
+				break;
 			}
-		} catch(Exception e) {
-			logger.severe(e.getMessage());
-		}
+		} 
 
 		return links;
 	}
